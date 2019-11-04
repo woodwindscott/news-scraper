5f11b47c6c6186e3272c35284026203e75c80d7e
Scott Moore <woodwindscott@yahoo.com>
2019-11-03 19:40:52 -0500

    Functionality to return to the home page after scraping new articles and reloading after clearning articles

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
             }).then(
             function() {
                 console.log("Scrape complete");
+                location.reload();
             }
         );
     });
@@ -67,78 +68,11 @@ $(document).ready(function () {
                     }
                 );
 
-
-
-
-
             } // End else statement
         });
     });
 
-    // app.post("/submit", function(req, res) {
-    //     // Create a new Note in the db
-    //     db.Note.create(req.body)
-    //         .then(function(dbNote) {
-    //         // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
-    //         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-    //         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-    //         return db.User.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
-    //         })
-    //         .then(function(dbUser) {
-    //         // If the User was updated successfully, send it back to the client
-    //         res.json(dbUser);
-    //         })
-    //         .catch(function(err) {
-    //         // If an error occurs, send it back to the client
-    //         res.json(err);
-    //         });
-    //     });     
- 
 
-
-
-        
-        // // Send the PUT request.
-        // var id = $(this).attr("data-id");
-        // console.log(id);
-
-        // var savingArticle = {
-        //     isSaved: true
-        //     };
-
-        // $.ajax("/api/notes/" + id, {
-        //     type: "PUT",
-        //     data: savingArticle
-        //     }).then(
-        //     function() {
-        //         console.log("Article has been saved.");
-        //         // Reload the page to remove the newly saved article from the listings
-        //         location.reload();
-        //     }
-        // );
-    // });
-
-    // // Saves an article when button is clicked
-    // $(".notes").on("click", function(event) {
-    //     // Send the PUT request.
-    //     var id = $(this).attr("data-id");
-    //     console.log(id);
-
-    //     var savingArticle = {
-    //         isSaved: true
-    //         };
-
-    //     $.ajax("/api/notes/" + id, {
-    //         type: "PUT",
-    //         data: savingArticle
-    //         }).then(
-    //         function() {
-    //             console.log("Article has been saved.");
-    //             // Reload the page to remove the newly saved article from the listings
-    //             location.reload();
-    //         }
-    //     );
-    // });
 
     // Removes an article from saved when button is clicked
     $(".unsave").on("click", function(event) {
@@ -173,6 +107,7 @@ $(document).ready(function () {
             }).then(
             function() {
                 console.log("Clear articles: complete");
+                location.reload();
             }
         );
     });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -50,8 +50,8 @@ module.exports = function(app) {
                 });
             });
 
-        // Send a message to the client
-        res.send("Scrape Complete");
+        // Redirect to the home page
+        return res.redirect('/');
         });
     });
 
